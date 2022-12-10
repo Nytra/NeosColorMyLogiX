7e69434795cdc3efc68ae012ace07b5151e3bd04
Nytra <samueltscott@gmail.com>
2022-12-10 13:44:41 +0000

    Update AssemblyInfo.cs

--- MotionBlurDisable/Properties/AssemblyInfo.cs
+++ MotionBlurDisable/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ColorMyLogixNodes")]
-[assembly: AssemblyCopyright("Copyright © Nytra 2021")]
+[assembly: AssemblyCopyright("Copyright © Nytra 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
