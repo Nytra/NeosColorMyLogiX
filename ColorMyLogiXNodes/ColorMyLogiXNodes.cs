d569f913ced965a5a9a4c53510099ce3226ddeba
Sam Scott <samueltscott@gmail.com>
2022-12-13 19:59:39 +0000

    Make error'd nodes have a noticeable color. Add config option for error'd node color. A bit of code cleanup.

--- ColorMyLogiXNodes/ColorMyLogiXNodes.cs
+++ ColorMyLogiXNodes/ColorMyLogiXNodes.cs
@@ -1,5 +1,6 @@
 ﻿using FrooxEngine;
 using FrooxEngine.LogiX;
+using FrooxEngine.UIX;
 using HarmonyLib;
 using NeosModLoader;
 using BaseX;
@@ -14,18 +15,18 @@ namespace ColorMyLogixNodes
         public override string Version => "1.0.0";
         public override string Link => "https://github.com/Nytra/NeosColorMyLogiXNodes";
 
-        //private static bool _first_trigger = false;
-
         private const string IMAGE_SLOT_TAG = "ColorMyLogiXNodes.ColorSet";
 
         public static ModConfiguration Config;
 
         [AutoRegisterConfigKey]
-        private static ModConfigurationKey<bool> MOD_ENABLED = new ModConfigurationKey<bool>("Mod enabled.", "", () => true);
+        private static ModConfigurationKey<bool> MOD_ENABLED = new ModConfigurationKey<bool>("Mod enabled:", "", () => true);
         [AutoRegisterConfigKey]
-        private static ModConfigurationKey<bool> USE_RANDOM_COLORS = new ModConfigurationKey<bool>("Use random colors. If false, mod uses the configured color.", "", () => true);
+        private static ModConfigurationKey<bool> USE_RANDOM_COLORS = new ModConfigurationKey<bool>("Use random colors (Does not affect error'd nodes):", "", () => true);
         [AutoRegisterConfigKey]
-        private static ModConfigurationKey<BaseX.color> CONFIGURED_COLOR = new ModConfigurationKey<color>("Configured color.", "", () => new BaseX.color(1.0f, 1.0f, 1.0f, 0.8f));
+        private static ModConfigurationKey<BaseX.color> NODE_COLOR = new ModConfigurationKey<BaseX.color>("Node color:", "", () => new BaseX.color(1.0f, 1.0f, 1.0f, 0.8f));
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<BaseX.color> NODE_ERROR_COLOR = new ModConfigurationKey<BaseX.color>("Node error color:", "", () => new BaseX.color(3.0f, 0.5f, 0.5f, 0.8f));
 
         private static System.Random rng = new System.Random();
 
@@ -39,49 +40,73 @@ namespace ColorMyLogixNodes
             harmony.PatchAll();
         }
 
+        private static void TrySetTag(Slot s, string tag)
+        {
+            try
+            {
+                s.Tag = tag;
+            }
+            catch (Exception e)
+            {
+                Error($"Error occurred while trying to set Image Slot Tag.\nError message: {e.Message}");
+            }
+        }
+
+        private static void TrySetColor(Image image, BaseX.color color)
+        {
+            try
+            {
+                image.Tint.Value = color;
+            }
+            catch (Exception e)
+            {
+                Error($"Error occurred while trying to set Image Tint Value.\nError message: {e.Message}");
+            }
+        }
+
         [HarmonyPatch(typeof(LogixNode))]
         [HarmonyPatch(nameof(LogixNode.GenerateVisual))]
         class Patch2
         {
             static void Postfix(LogixNode __instance)
             {
-                //if (!_first_trigger) _first_trigger = true;
-
                 if (Config.GetValue(MOD_ENABLED) == true && __instance.Slot != null)
                 {
-                    var ImageSlot = __instance.Slot.FindChild((Slot c) => c.Name == "Image");
-                    if (ImageSlot != null)
+                    var imageSlot = __instance.Slot.FindChild((Slot c) => c.Name == "Image");
+                    if (imageSlot != null)
                     {
-                        if (ImageSlot.ReferenceID.User == __instance.LocalUser.AllocationID)
+                        if (imageSlot.ReferenceID.User == __instance.LocalUser.AllocationID) // check logix node visual is allocated to the local user
                         {
-                            if (ImageSlot.Tag != IMAGE_SLOT_TAG)
+                            if (__instance.Enabled == false)
                             {
-                                try
+                                if (imageSlot.Tag == IMAGE_SLOT_TAG) // idk if this will ever be true, because I think Neos changes the tag to "Disabled" when the node breaks.
                                 {
-                                    ImageSlot.Tag = IMAGE_SLOT_TAG;
-                                }
-                                catch (Exception e)
-                                {
-                                    Error($"Error occurred while trying to set Image Slot Tag.\nError message: {e.Message}");
+                                    TrySetTag(imageSlot, "");
                                 }
 
-                                var Image = ImageSlot.GetComponent<FrooxEngine.UIX.Image>();
-                                if (Image != null)
+                                var image = imageSlot.GetComponent<Image>();
+                                if (image != null)
                                 {
-                                    try
+                                    TrySetColor(image, Config.GetValue(NODE_ERROR_COLOR));
+                                }
+                            }
+                            else
+                            {
+                                if (imageSlot.Tag != IMAGE_SLOT_TAG)
+                                {
+                                    TrySetTag(imageSlot, IMAGE_SLOT_TAG);
+
+                                    var image = imageSlot.GetComponent<Image>();
+                                    if (image != null)
                                     {
                                         if (Config.GetValue(USE_RANDOM_COLORS) == true)
                                         {
-                                            Image.Tint.Value = new BaseX.color(rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, 0.8f);
+                                            TrySetColor(image, new BaseX.color(rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, 0.8f));
                                         }
                                         else
                                         {
-                                            Image.Tint.Value = Config.GetValue(CONFIGURED_COLOR);
+                                            TrySetColor(image, Config.GetValue(NODE_COLOR));
                                         }
-                                    }
-                                    catch (Exception e)
-                                    {
-                                        Error($"Error occurred while trying to set Image Tint Value.\nError message: {e.Message}");
                                     }
                                 }
                             }
