e48d6034790c84667028d9bdea30a60005569930
Sam Scott <samueltscott@gmail.com>
2022-12-23 23:23:08 +0000

    Add node color mode config option (enum). Allows to further change the way the mod generates colors. Add ways to generate color based on node name, full type name, by RefID, or by true randomness. The mod will also no longer color cast nodes.

--- ColorMyLogiXNodes/ColorMyLogiXNodes.cs
+++ ColorMyLogiXNodes/ColorMyLogiXNodes.cs
@@ -6,6 +6,7 @@ using NeosModLoader;
 using BaseX;
 using System;
 using FrooxEngine.LogiX.Math;
+using System.Collections.Generic;
 
 namespace ColorMyLogixNodes
 {
@@ -13,23 +14,34 @@ namespace ColorMyLogixNodes
     {
         public override string Name => "ColorMyLogiXNodes";
         public override string Author => "Nytra";
-        public override string Version => "1.0.0-alpha5";
+        public override string Version => "1.0.0-alpha6";
         public override string Link => "https://github.com/Nytra/NeosColorMyLogiXNodes";
 
         private const string COLOR_SET_TAG = "ColorMyLogiXNodes.ColorSet";
+
+        private enum NodeColorModeEnum
+        {
+            UseConfig,
+            ByNodeName,
+            ByFullTypeName,
+            ByRefID,
+            TrueRandom
+        }
 
         public static ModConfiguration Config;
 
         [AutoRegisterConfigKey]
         private static ModConfigurationKey<bool> MOD_ENABLED = new ModConfigurationKey<bool>("Mod enabled:", "", () => true);
         [AutoRegisterConfigKey]
-        private static ModConfigurationKey<bool> USE_RANDOM_COLORS = new ModConfigurationKey<bool>("Use random colors (Does not affect error'd nodes):", "", () => true);
+        private static ModConfigurationKey<NodeColorModeEnum> NODE_COLOR_MODE = new ModConfigurationKey<NodeColorModeEnum>("Node color mode (Does not affect error nodes):", "", () => NodeColorModeEnum.UseConfig);
         [AutoRegisterConfigKey]
-        private static ModConfigurationKey<BaseX.color> NODE_COLOR = new ModConfigurationKey<BaseX.color>("Node color:", "", () => new BaseX.color(1.0f, 1.0f, 1.0f, 0.8f));
+        private static ModConfigurationKey<BaseX.color> NODE_COLOR = new ModConfigurationKey<BaseX.color>("Node color (For UseConfig mode):", "", () => new BaseX.color(1.0f, 1.0f, 1.0f, 0.8f));
         [AutoRegisterConfigKey]
         private static ModConfigurationKey<BaseX.color> NODE_ERROR_COLOR = new ModConfigurationKey<BaseX.color>("Node error color:", "", () => new BaseX.color(3.0f, 0.5f, 0.5f, 0.8f));
 
-        private static System.Random rng = new System.Random();
+        private static System.Random rng;
+
+        private const bool COLOR_BY_NODE_NAME = true;
 
         public override void OnEngineInit()
         {
@@ -65,71 +77,61 @@ namespace ColorMyLogixNodes
             }
         }
 
-        private static void TrySetValueFieldValue(ValueField<BaseX.color> valueField, BaseX.color color)
-        {
-            try
-            {
-                valueField.Value.Value = color;
-            }
-            catch (Exception e)
-            {
-                Error($"Error occurred while trying to set ValueField Value Value.\nError message: {e.Message}");
-            }
-        }
-
         [HarmonyPatch(typeof(LogixNode))]
         [HarmonyPatch("GenerateUI")]
         class Patch_LogixNode_GenerateUI
         {
-            static void Postfix(Slot root)
+            static void Postfix(LogixNode __instance, Slot root)
             {
-                if (Config.GetValue(MOD_ENABLED) == true && root != null && root.ReferenceID.User == root.LocalUser.AllocationID) // check logix node visual is allocated to the local user
+                // only run if the logix node visual slot is allocated to the local user
+                if (Config.GetValue(MOD_ENABLED) == true && root != null && root.ReferenceID.User == root.LocalUser.AllocationID)
                 {
+                    // don't apply custom color to cast nodes, because it makes it confusing to read the data types
+                    if (__instance.Name.Contains("CastClass")) return;
+
                     if (root.Tag != COLOR_SET_TAG)
                     {
                         var imageSlot = root.FindChild((Slot c) => c.Name == "Image");
                         if (imageSlot != null)
                         {
-                            if (root.Tag == "Disabled")
+                            var image = imageSlot.GetComponent<Image>();
+                            if (image != null)
                             {
-                                var image = imageSlot.GetComponent<Image>();
-                                if (image != null)
+                                if (root.Tag == "Disabled")
                                 {
                                     TrySetImageTint(image, Config.GetValue(NODE_ERROR_COLOR));
                                 }
-                            }
-                            else
-                            {
-                                //var valueField = root.Parent.GetComponent<ValueField<BaseX.color>>();
-                                var image = imageSlot.GetComponent<Image>();
-                                //if (valueField == null)
-                                //{
-                                if (image != null)
+                                else
                                 {
-                                    //valueField = root.Parent.AttachComponent<ValueField<BaseX.color>>();
-                                    //valueField.Persistent = false;
-
-                                    if (Config.GetValue(USE_RANDOM_COLORS) == true)
+                                    BaseX.color colorToSet = Config.GetValue(NODE_COLOR);
+                                    rng = null;
+                                    switch (Config.GetValue(NODE_COLOR_MODE))
                                     {
-                                        //TrySetValueFieldValue(valueField, new BaseX.color(rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, 0.8f));
-                                        TrySetImageTint(image, new BaseX.color(rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, 0.8f));
+                                        case NodeColorModeEnum.UseConfig:
+                                            colorToSet = Config.GetValue(NODE_COLOR);
+                                            break;
+                                        case NodeColorModeEnum.ByNodeName:
+                                            rng = new System.Random(LogixHelper.GetNodeName(__instance.GetType()).GetHashCode());
+                                            break;
+                                        case NodeColorModeEnum.ByFullTypeName:
+                                            rng = new System.Random(__instance.GetType().FullName.GetHashCode());
+                                            break;
+                                        case NodeColorModeEnum.ByRefID:
+                                            rng = new System.Random(root.Parent.ReferenceID.GetHashCode());
+                                            break;
+                                        case NodeColorModeEnum.TrueRandom:
+                                            rng = new System.Random();
+                                            break;
+                                        default:
+                                            break;
                                     }
-                                    else
+                                    if (rng != null)
                                     {
-                                        //TrySetValueFieldValue(valueField, Config.GetValue(NODE_COLOR));
-                                        TrySetImageTint(image, Config.GetValue(NODE_COLOR));
+                                        colorToSet = new BaseX.color(rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, rng.Next(101) / 100.0f, 0.8f);
                                     }
-
-                                    TrySetTag(imageSlot, COLOR_SET_TAG);
+                                    TrySetImageTint(image, colorToSet);
+                                    TrySetTag(root, COLOR_SET_TAG);
                                 }
-                                //}
-                                //else
-                                //{
-                                    //if (image != null && image.Tint.Value != valueField.Value.Value)
-                                    //{
-                                        //TrySetImageTint(image, valueField.Value.Value);
-                                    //}
-                                //}
                             }
                         }
                     }
