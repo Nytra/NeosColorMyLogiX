e238493dccc8ff9e81bd09e944b3f4e23dada873
Sam Scott <samueltscott@gmail.com>
2023-04-08 05:46:07 +0100

    Update AssemblyInfo.cs

--- ColorMyLogiXNodes/Properties/AssemblyInfo.cs
+++ ColorMyLogiXNodes/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColorMyLogiXNodes")]
+[assembly: AssemblyTitle("ColorMyLogiX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ColorMyLogiXNodes")]
+[assembly: AssemblyProduct("ColorMyLogiX")]
 [assembly: AssemblyCopyright("Copyright © Nytra 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
